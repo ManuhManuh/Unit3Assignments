a0b0491165f85c7108cb6c6dd5fef2c7fefaac2a
ManuhManuh <cara.sterling200@gmail.com>
2022-06-09 22:49:42 -0600

    Changed shake, cosmetic tweaks
    Changed shake to rotate rather than move position; changed capsule to cylinder, added texture to sphere so shake is visible

--- Assets/Assignment3/Scripts/ShakeableObject.cs
+++ Assets/Assignment3/Scripts/ShakeableObject.cs
@@ -6,30 +6,16 @@ public class ShakeableObject : MonoBehaviour
 {
     public bool IsShaking = false;
 
-    [SerializeField] private float shakeSpeed = 2.0f;
-    [SerializeField] private float shakeDistance = 7.0f;
+    [SerializeField] private float shakeSpeed = 20.0f;
+    [SerializeField] private float startAngle = 0.5f;
 
-    private float m_startX;
-    private float m_startY;
-    private float m_startZ;
-
-    private void Awake()
-    {
-        m_startX = transform.position.x;
-        m_startY = transform.position.y;
-        m_startZ = transform.position.z;
-    }
     private void Update()
     {
         if (IsShaking)
         {
+            float angleToMove = Mathf.Sin(Time.time * shakeSpeed);
 
-            float newX = m_startX + Mathf.Sin(shakeSpeed * Time.deltaTime) * shakeDistance;
-            float newY = m_startY + Mathf.Sin(shakeSpeed * Time.deltaTime) * shakeDistance;
-            float newZ = m_startZ + Mathf.Sin(shakeSpeed * Time.deltaTime) * shakeDistance;
-
-
-            transform.position = new Vector3(newX, newY, newZ);
+            this.transform.localRotation = Quaternion.Euler(angleToMove, 0, 0);
 
         }
     }
